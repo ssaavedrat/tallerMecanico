56f234e9ff632ce677138ae351e55b09762e9ed5
Sergio Saavedra <sergio.saavedra@ug.uchile.cl>
2023-10-13 07:33:38 -0300

    fix rm contact view and add contact scaffold

---
+++ app/models/contact.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: contacts
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  email      :string
+#  message    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Contact < ApplicationRecord
+    # Validations
+    validates :name, presence: true
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :message, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  resources :contacts
   get 'pages/home'
   get 'pages/about'
-  get 'pages/contact'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'pages#home'
   get 'sobre', to: 'pages#about'
-  get 'contacto', to: 'pages#contact'
+
 end
